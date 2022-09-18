c7b89603a19c5d961092f430632e15727b0cb12a
rezakali <rezakali1993@gmail.com>
2022-09-19 01:38:24 +0530

    Added Validation with yup

--- models/books.js
+++ models/books.js
@@ -1,22 +1,42 @@
-const mongoose = require("mongoose")
-const Author = require("./author")
+const mongoose = require("mongoose");
+const Author = require("./author");
+const yup = require("yup");
 
 /***BOOK SCHEMA */
 
 const BookSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        minlength:3,
-        maxlength:50
-    },
-    author:Author.schema,
-    genre:{
-        type:String,
-        required:true,
-        minlength:3,
-        maxlength:30
-    }
-})
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  author: Author.schema,
+  genre: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 30,
+  },
+});
 
-module.exports = new mongoose.model("Book", BookSchema)
+const validateBook = (book) => {
+  const schema = yup.object().shape({
+    bookName: yup.string().required().min(3).max(50),
+    authorName: yup.string().required().min(3).max(30),
+    authorAge: yup.number().required().min(20).max(85),
+    genre: yup.string().required().min(3).max(30),
+  });
+
+  return schema
+    .validate(book)
+    .then((book) => book)
+    .catch((error) => {
+      return {
+        message: error.message,
+      };
+    });
+};
+
+exports.Book = new mongoose.model("Book", BookSchema);
+exports.validateBook = validateBook;
--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const Book = require("../models/books");
+const { Book, validateBook } = require("../models/books");
 
 /**POST METHOD */
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
+  const error = await validateBook(req.body);
+  if (error.message) res.status(400).send(error.message);
   book = new Book({
     name: req.body.bookName,
     author: {
@@ -13,12 +15,14 @@ router.post("/", (req, res) => {
     },
     genre: req.body.genre,
   });
-  book.save().then(book => {
-    res.send(book)
-  }).catch(error => {
-    res.status(500).send("book was not stored in db")
-  })
+  book
+    .save()
+    .then((book) => {
+      res.send(book);
+    })
+    .catch((error) => {
+      res.status(500).send("book was not stored in db");
+    });
 });
-
 
 module.exports = router;
